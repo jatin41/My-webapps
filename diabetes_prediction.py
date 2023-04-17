3ab06868dcf05267a07acf8c272e0ad4b26e9827
jatin41 <84264122+jatin41@users.noreply.github.com>
2023-04-17 09:02:32 +0530

    Update diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 import streamlit as st
-
+st.header("Diabetes prediction")
 # loading the diabetes dataset to a pandas DataFrame
 diabetes_dataset = pd.read_csv('diabetes (1).csv')
 
